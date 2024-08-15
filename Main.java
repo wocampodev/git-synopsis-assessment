a54a9ca34b35f51de3248bff46759217cd565bd6
JeanDeveloper <jecachusa.1996@gmail.com>
2024-08-14 21:48:01 -0500

    add Main.java

---
+++ Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    
+    // sum two number
+    public static int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static void main(String[] args) {
+        int number1 = 5;
+        int number2 = 10;
+
+        int result = sum(number1, number2);
+
+        System.out.println("La suma de " + number1 + " y " + number2 + " es: " + result);
+    }
+}
